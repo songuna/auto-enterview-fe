bc68cd7cb8f896dfc4d39c4ef335cc3d3bc9cc5b
You-NaSong <twins7534@naver.com>
2024-07-24 21:18:59 +0900

    resume.ts 로직 최최종

--- src/axios/http/resume.ts
+++ src/axios/http/resume.ts
@@ -9,5 +9,6 @@ const resumeEndpoint = (method: Method, candidateKey: string, body?: any, config
 export const getResume = (candidateKey: string) => resumeEndpoint("get", candidateKey);
 export const postResume = (candidateKey: string, body: any, config?: any) =>
   resumeEndpoint("post", candidateKey, body, config);
-export const putResume = (candidateKey: string, body: any, config?: any) => resumeEndpoint("put", candidateKey, body, config);
-export const deleteResume = (candidateKey: string,) => resumeEndpoint("delete", candidateKey);
+export const putResume = (candidateKey: string, body: any, config?: any) =>
+  resumeEndpoint("put", candidateKey, body, config);
+export const deleteResume = (candidateKey: string) => resumeEndpoint("delete", candidateKey);
